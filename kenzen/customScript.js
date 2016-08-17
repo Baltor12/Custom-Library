e5950f79731617f642118080d4287442d05c4ac7
Baltor12 <balaji.aviator@gmail.com>
2016-08-18 00:10:38 +0300

    kenzen_push_3

--- kenzen/customScript.js
+++ kenzen/customScript.js
@@ -1,79 +1,147 @@
-function toggle(button) { 
-            if ( button.getAttribute("value") == 'ON' ){ 
-                switch(button.getAttribute("id")){ 
-                    case 'scent1': 
-                        window.location.href = window.location.origin + '/scent1=ON'; 
-                        break; 
-                    case 'scent2':                     
-                        window.location.href = window.location.origin + '/scent2=ON'; 
-                        break; 
-                    case 'scent3': 
-                        window.location.href = window.location.origin + '/scent3=ON'; 
-                        break; 
-                    case 'scent4': 
-                        window.location.href = window.location.origin + '/scent4=ON'; 
-                        break; 
-                    case 'scent5': 
-                        window.location.href = window.location.origin + '/scent5=ON'; 
-                        break; 
-                    case 'scent6': 
-                        window.location.href = window.location.origin + '/scent6=ON'; 
-                        break; 
-                    case 'scent7': 
-                        window.location.href = window.location.origin + '/scent7=ON'; 
-                        break; 
-                    case 'scent8': 
-                        window.location.href = window.location.origin + '/scent8=ON'; 
-                        break;
-                    case 'temp1': 
-                        window.location.href = window.location.origin + '/temp1=ON'; 
-                        break;
-                    case 'temp2': 
-                        window.location.href = window.location.origin + '/temp2=ON'; 
-                        break;
-                    case 'temp3': 
-                        window.location.href = window.location.origin + '/temp3=ON'; 
-                        break;
-                }
-            } else{
-                switch(button.getAttribute("id")){ 
-                    case 'scent1': 
-                        window.location.href = window.location.origin + '/scent1=OFF';
-                        break; 
-                    case 'scent2': 
-                        window.location.href = window.location.origin + '/scent2=OFF';
-                        break; 
-                    case 'scent3': 
-                        window.location.href = window.location.origin + '/scent3=OFF'; 
-                        break; 
-                    case 'scent4': 
-                        window.location.href = window.location.origin + '/scent4=OFF'; 
-                        break; 
-                    case 'scent5': 
-                        window.location.href = window.location.origin + '/scent5=OFF'; 
-                        break; 
-                    case 'scent6': 
-                        window.location.href = window.location.origin + '/scent6=OFF'; 
-                        break; 
-                    case 'scent7': 
-                        window.location.href = window.location.origin + '/scent7=OFF'; 
-                        break; 
-                    case 'scent8': 
-                        window.location.href = window.location.origin + '/scent8=OFF'; 
-                        break;
-                    case 'temp1': 
-                        window.location.href = window.location.origin + '/temp1=OFF'; 
-                        break;
-                    case 'temp2':
-                        window.location.href = window.location.origin + '/temp2=OFF'; 
-                        break;
-                    case 'temp3': 
-                        window.location.href = window.location.origin + '/temp3=OFF'; 
-                        break;
-                }
-            }
+function toggle(button) {
+    if (button.getAttribute("value") == 'ON') {
+        switch (button.getAttribute("id")) {
+            case 'scent1':
+                $.post('on?comp=blower1', function (data, status) {
+                    document.getElementsById('scent1').setAttribute('value', 'OFF');
+                    document.getElementsById('scent1').style.opacity = "0.5";
+                    document.getElementsById('scent1').style.outline = "thick solid red";
+                });
+                break;
+            case 'scent2':
+                $.post('on?comp=blower2', function (data, status) {
+                    document.getElementsById('scent2').setAttribute('value', 'OFF');
+                    document.getElementsById('scent2').style.opacity = "0.5";
+                    document.getElementsById('scent2').style.outline = "thick solid red";
+                });
+                break;
+            case 'scent3':
+                $.post('on?comp=blower3', function (data, status) {
+                    document.getElementsById('scent3').setAttribute('value', 'OFF');
+                    document.getElementsById('scent3').style.opacity = "0.5";
+                    document.getElementsById('scent3').style.outline = "thick solid red";
+                });
+                break;
+            case 'scent4':
+                $.post('on?comp=blower4', function (data, status) {
+                    document.getElementsById('scent4').setAttribute('value', 'OFF');
+                    document.getElementsById('scent4').style.opacity = "0.5";
+                    document.getElementsById('scent4').style.outline = "thick solid red";
+                });
+                break;
+            case 'scent5':
+                $.post('on?comp=blower5', function (data, status) {
+                    document.getElementsById('scent5').setAttribute('value', 'OFF');
+                    document.getElementsById('scent5').style.opacity = "0.5";
+                    document.getElementsById('scent5').style.outline = "thick solid red";
+                });
+                break;
+            case 'scent6':
+                $.post('on?comp=blower6', function (data, status) {
+                    document.getElementsById('scent6').setAttribute('value', 'OFF');
+                    document.getElementsById('scent6').style.opacity = "0.5";
+                    document.getElementsById('scent6').style.outline = "thick solid red";
+                });
+                break;
+            case 'scent7':
+                $.post('on?comp=blower7', function (data, status) {
+                    document.getElementsById('scent7').setAttribute('value', 'OFF');
+                    document.getElementsById('scent7').style.opacity = "0.5";
+                    document.getElementsById('scent7').style.outline = "thick solid red";
+                });
+                break;
+            case 'scent8':
+                $.post('on?comp=blower8', function (data, status) {
+                    document.getElementsById('scent8').setAttribute('value', 'OFF');
+                    document.getElementsById('scent8').style.opacity = "0.5";
+                    document.getElementsById('scent8').style.outline = "thick solid red";
+                });
+                break;
+            case 'temp1':
+                $.post('template?temp=template1', function (data, status) {
+                    document.getElementsById('temp1').style.opacity = "0.5";
+                    document.getElementsById('temp1').style.outline = "thick solid red";
+                });
+                break;
+            case 'temp2':
+                $.post('template?temp=template2', function (data, status) {
+                    document.getElementsById('temp2').style.opacity = "0.5";
+                    document.getElementsById('temp2').style.outline = "thick solid red";
+                });
+                break;
+            case 'temp3':
+                $.post('template?temp=template3', function (data, status) {
+                    document.getElementsById('temp3').style.opacity = "0.5";
+                    document.getElementsById('temp3').style.outline = "thick solid red";
+                });
+                break;
         }
-        function load(){
+    } else {
+        switch (button.getAttribute("id")) {
+            case 'scent1':
+                $.post('off?comp=blower1', function (data, status) {
+                    document.getElementsById('scent1').setAttribute('value', 'ON');
+                    document.getElementsById('scent1').style.opacity = "1";
+                    document.getElementsById('scent1').style.outline = "none";
+                });
+                break;
+            case 'scent2':
+                $.post('off?comp=blower2', function (data, status) {
+                    document.getElementsById('scent2').setAttribute('value', 'ON');
+                    document.getElementsById('scent2').style.opacity = "1";
+                    document.getElementsById('scent2').style.outline = "none";
+                });
+                break;
+            case 'scent3':
+                $.post('off?comp=blower3', function (data, status) {
+                    document.getElementsById('scent3').setAttribute('value', 'ON');
+                    document.getElementsById('scent3').style.opacity = "1";
+                    document.getElementsById('scent3').style.outline = "none";
+                });
+                break;
+            case 'scent4':
+                $.post('off?comp=blower4', function (data, status) {
+                    document.getElementsById('scent4').setAttribute('value', 'ON');
+                    document.getElementsById('scent4').style.opacity = "1";
+                    document.getElementsById('scent4').style.outline = "none";
+                });
+                break;
+            case 'scent5':
+                $.post('off?comp=blower5', function (data, status) {
+                    document.getElementsById('scent5').setAttribute('value', 'ON');
+                    document.getElementsById('scent5').style.opacity = "1";
+                    document.getElementsById('scent5').style.outline = "none";
+                });
+                break;
+            case 'scent6':
+                $.post('off?comp=blower6', function (data, status) {
+                    document.getElementsById('scent6').setAttribute('value', 'ON');
+                    document.getElementsById('scent6').style.opacity = "1";
+                    document.getElementsById('scent6').style.outline = "none";
+                });
+                break;
+            case 'scent7':
+                $.post('off?comp=blower7', function (data, status) {
+                    document.getElementsById('scent7').setAttribute('value', 'ON');
+                    document.getElementsById('scent7').style.opacity = "1";
+                    document.getElementsById('scent7').style.outline = "none";
+                });
+                break;
+            case 'scent8':
+                $.post('off?comp=blower8', function (data, status) {
+                    document.getElementsById('scent8').setAttribute('value', 'ON');
+                    document.getElementsById('scent8').style.opacity = "1";
+                    document.getElementsById('scent8').style.outline = "none";
+                });
+                break;
+            case 'temp1':
+            case 'temp2':
+            case 'temp3':
+                break;
+        }
+    }
+}
+        /*function load(){
             var elems = document.body.getElementsByTagName("*");
             for(i=0; i<elems.length; i++){
                 if((elems[i].getAttribute("id") != null ) && (elems[i].getAttribute("id") != '') && (elems[i].tagName == 'DIV')){
@@ -83,4 +151,4 @@ function toggle(button) {
                     }                    
                 }
             }
-        }
+        }*/
