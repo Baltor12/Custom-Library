76374a3fd9e0925239a54ef1d57a3f78501764ae
Baltor12 <balaji.aviator@gmail.com>
2016-09-29 21:59:06 +0300

    kenzen_push_9

--- kenzen/customScript.js
+++ kenzen/customScript.js
@@ -168,7 +168,7 @@ window.setInterval(function () {
 
 function getStatus() {
     //Get the status of all the bloweres from the device and activate the button accordingly
-    $get('status', function (data, status) {
+    $.get('status', function (data, status) {
         console.log(data);
         var json = JSON.parse(data);
         for (var key in json) {
